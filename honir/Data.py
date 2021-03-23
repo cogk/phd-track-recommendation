e947df26865222bb5bb630f401d477b69e23284b
Corentin Forler <cogk42@users.noreply.github.com>
2021-03-23 10:36:35 +0100

    estimate ratings of tag pairs

--- honir/Data.py
+++ honir/Data.py
@@ -15,6 +15,9 @@ def normalize_rating(n_stars: float) -> float:
 class Data:
     def __init__(self, dir='data/ml-20m'):
         print('loading…')
+        # Nettoyage : 6.2
+
+        # +2 tags
         self.movies = import_csv(dir + '/movies.csv', {
             'movieId': 'int64',
             'title': 'str',
@@ -30,6 +33,7 @@ class Data:
         })
         print(len(self.ratings), 'ratings')
 
+        # 5 utilisateurs différents, 2 films différents
         self.tags = import_csv(dir + '/tags.csv', {
             'userId': 'int64',
             'movieId': 'int64',
@@ -63,3 +67,49 @@ class Data:
         )
 
         return mean_rating_by_user_for_tag
+
+    def estimated_rating_for_tags_for_user(self, userId, tagA, tagB):
+        T, R, M = self.tags, self.ratings, self.movies
+
+        ids_movies_with_both_tags = dd.merge(
+            T[T.tag == tagA],
+            T[T.tag == tagB],
+            on="movieId"
+        )[('movieId')]
+
+        ratings = dd.merge(
+            R[R.userId == userId],
+            ids_movies_with_both_tags,
+            on='movieId'
+        )['rating']
+
+        normalized_ratings = normalize_rating(ratings)
+
+        mean_rating_by_user_for_tag_pair = (
+            normalized_ratings.sum().compute() / len(ids_movies_with_both_tags)
+        )
+
+        return mean_rating_by_user_for_tag_pair
+
+    def estimated_rating_for_tags_for_user_filter(self, userId, f1, f2):
+        T, R, M = self.tags, self.ratings, self.movies
+
+        ids_movies_with_both_tags = dd.merge(
+            T[f1],
+            T[f2],
+            on="movieId"
+        )[('movieId')]
+
+        ratings = dd.merge(
+            R[R.userId == userId],
+            ids_movies_with_both_tags,
+            on='movieId'
+        )['rating']
+
+        normalized_ratings = normalize_rating(ratings)
+
+        mean_rating_by_user_for_tag_pair = (
+            normalized_ratings.sum().compute() / len(ids_movies_with_both_tags)
+        )
+
+        return mean_rating_by_user_for_tag_pair
--- honir/__init__.py
+++ honir/__init__.py
@@ -3,12 +3,24 @@ from honir.Data import Data
 
 def main():
     d = Data()
-    print(d.movies.head())
-    print()
-    print(d.ratings.head())
-    print()
-    print(d.tags.head())
-    print()
+    T, R, M = d.tags, d.ratings, d.movies
+    # print(d.movies.head())
+    # print()
+    # print(d.ratings.head())
+    # print()
+    # print(d.tags.head())
+    # print()
 
-    est_r = d.estimated_rating_for_tag_for_user(11, 'dark hero')
-    print("-> estimated rating for tag 'dark hero' for user 11", est_r)
+    est_r_ta = d.estimated_rating_for_tag_for_user(11, 'dark hero')
+    print("-> weight('dark hero', user=11)", est_r_ta)
+
+    est_r_ta_connective_tb = d.estimated_rating_for_tags_for_user(
+        11, 'dark hero', 'surreal')
+    w_ta_connective_tb = est_r_ta_connective_tb - est_r_ta
+    print("-> weight('dark hero' & 'surreal', user=11)", w_ta_connective_tb)
+
+    est_r_ta_connective_not_tb = d.estimated_rating_for_tags_for_user_filter(
+        11, T.tag == 'dark hero', T.tag != 'surreal')
+    w_ta_connective_not_tb = est_r_ta_connective_not_tb - est_r_ta
+    print("-> weight('dark hero' but not 'surreal', user=11)",
+          w_ta_connective_not_tb)
